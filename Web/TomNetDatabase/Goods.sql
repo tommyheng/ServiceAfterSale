2de7592009b82d52d38ce6447b0f13bf9db62f6c
文明 汤 <12tommy@163.com>
2019-07-15 14:34:15 +0800

    update db

--- Web/TomNetDatabase/Goods.sql
+++ Web/TomNetDatabase/Goods.sql
@@ -1,10 +1,7 @@
 ﻿-- 货品表
--- type 进货单 ：0, 维修详情：1
 CREATE TABLE [dbo].[Goods]
 (
 	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
     [Name] NVARCHAR(50) NOT NULL, 
-	[GtId] INT NOT NULL, 
-    [Type] INT NOT NULL
-
+    [GtId] INT NOT NULL
 )
--- Web/TomNetDatabase/GoodsType.sql
+++ Web/TomNetDatabase/GoodsType.sql
@@ -1,6 +1,8 @@
 ﻿-- 货品型号
+-- type 产品型(进货时候用) ：0, 维修配件(维修详情时用)：1
 CREATE TABLE [dbo].[GoodsType]
 (
 	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
-    [Name] NVARCHAR(50) NOT NULL
+	[Type] INT NOT NULL, 
+    [Name] NVARCHAR(50) NOT NULL 
 )
